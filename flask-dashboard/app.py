40718237a933a1fba6f653c7656614be99083a8f
Courtney Shearer <csheare@clemson.edu>
2017-10-14 01:38:01 -0400

    Connect to db

--- flask-dashboard/app.py
+++ flask-dashboard/app.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template, json, request, make_response
+from firebase import firebase
+#from .forms import FirePut
+
 app = Flask(__name__)
+firebase = firebase.FirebaseApplication('https://npcompete-54f8f.firebaseio.com', None)
+
 
 @app.route('/')
 def main():
     name = "Courtney"
     return render_template(
         'index.html',**locals())
-    return render_template('index.html')
+
+
+@app.route('/getData')
+def index():
+    result = firebase.get('/recipes', None)
+    print(result)
+    return json.dumps(result)
 
 if __name__ == "__main__":
     app.run(debug = True)
