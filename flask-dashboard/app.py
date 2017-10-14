99ceb30c5364457e597e67020617d8a91ee16884
Courtney Shearer <csheare@clemson.edu>
2017-10-13 23:03:48 -0400

    Updates to flask dashboard

--- flask-dashboard/app.py
+++ flask-dashboard/app.py
@@ -3,6 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
+    name = "Courtney"
+    return render_template(
+        'index.html',**locals())
     return render_template('index.html')
 
 if __name__ == "__main__":
