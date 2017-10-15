b3220063800220a5045998c679afedfe64d84abe
Angelo Carrabba <acarrabba6@gmail.com>
2017-10-15 06:55:32 -0400

    converted everything to react

--- flask-dashboard/Application/src/MainResult.Component.tsx
+++ flask-dashboard/Application/src/MainResult.Component.tsx
@@ -1,79 +1,155 @@
 import React from 'react';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import axios from 'axios';
 import FlatButton from 'material-ui/FlatButton';
+import { GridList, GridTile } from 'material-ui';
 
-
-
-export interface MainResultContainer {
-    recipeName: string;
-}
 
 function setOne(index, url, name) {
   document.getElementById("name" + index).innerHTML = name;
   let img = document.getElementById("image" + index);
   img['src'] = url;
+}
 
+interface MainRecipeInformation {
+  recipeName: string
+  directions: string
+  ingredients: string
+  imageUrl: string
+}
+interface MainResultProps {
+  result: MainRecipeInformation
+}
+class MainResult extends React.Component<MainResultProps> {
+  render() {
+    return (
+      <Card style={{ margin: '4em auto', maxWidth: 650 }}>
+        <CardMedia>
+          <img src={this.props.result.imageUrl} />
+        </CardMedia>
+        <CardTitle title={this.props.result.recipeName} />
+        <CardText>
+          {this.props.result.directions}
+          <br />
+          {this.props.result.ingredients}
+        </CardText>
+      </Card>
+    );
+  }
+}
+
+interface TileData {
+  recipeName: string
+  imageUrl: string
+}
+interface ReferencedResultsProps {
+  tiles: Array<TileData>
+  changeRecipe: (string) => void
+}
+
+class ReferencedResults extends React.Component<ReferencedResultsProps> {
+  render() {
+    return (
+      <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around" }}>
+        <GridList cellHeight={300} style={{ width: 1000, overflowY: 'auto' }} >
+          {
+            this.props.tiles.map((tile) => (
+              <a onClick={() => (this.props.changeRecipe(tile.recipeName))}>
+                <GridTile
+                  key={tile.imageUrl}
+                  title={tile.recipeName}
+                >
+                  <img src={tile.imageUrl} />
+                </GridTile>
+              </a>
+            ))
+          }
+        </GridList>
+      </div>
+    );
+  }
 }
 
 
-export default class MainResult extends React.Component<MainResultContainer> {
 
-    state: { directions: string , ingredients: string, image : string }
-    constructor(props) {
-        super(props)
-        this.state = { directions : "none" , ingredients: "none", image : "../static/assets/images/img-12.jpg"}
+export interface ResultContainer {
+  query: string;
+}
+
+export default class ResultOutput extends React.Component<ResultContainer> {
+
+  state: {
+    wasResult?: boolean,
+    query: string,
+    mainResult?: MainRecipeInformation,
+    referenceResult?: Array<TileData>
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      query: ""
     }
+  }
 
-
-    doSearch(recipeName: string) {
-        axios.get(`/recipes/` + recipeName )
-            .then(res => {
-                //const posts = res.data.data.map(obj => obj.data);
-                console.log(res);
-                // let ingredients = "Ingredients: "
-                // for (let property in res.data["ingredients"]) {
-                //   ingredients += property +" "+ res.data["ingredients"][property]['count'] + ", ";
-                // }
-                // if (ingredients.length > 0) {
-                //   ingredients = ingredients.slice(0, ingredients.length - 1);
-                // }
-
-              if(res.data !== undefined){
-                for (let i = 1; i <= 8; i++) {
-                   if (res.data["r" + i] !== undefined) {
-                     const ele = res.data["r" + i];
-                     setOne(i, ele["image-ref"], ele["name"])
-                   }
-                 }
-              }
-                this.setState({ directions: "Directions: " + res.data["directions"], ingredients: "Ingredients: " + res.data["ingredients"]["text"], image: "" + res.data["image"] });
-            });
-    }
-
-    componentWillReceiveProps(props) {
-        if (props.recipeName.length > 0) {
-            this.doSearch(props.recipeName)
+  doSearch(query: string) {
+    axios.get(`/recipes/` + query)
+      .then(res => {
+        console.log(res);
+        if (res.data === undefined || res.data === "No Recipe Found") {
+          this.setState({ wasResult: false, query: query });
+          return;
         }
-    }
 
-    render() {
-        return(
-              <Card
-                style={{
-                    margin: '4em auto',
-                    maxWidth: 650
-                }}
-              >
-                <CardMedia>
-                  <img src= {this.state.image} alt="" />
-                </CardMedia>
-                <CardTitle title= {this.props.recipeName} />
-                <CardText>
-                  {this.state.directions}
-                  {this.state.ingredients}
-                </CardText>
-              </Card>
-            );
+        let references: Array<TileData> = []
+        for (let i = 1; i <= 8; i++) {
+          const ele = res.data["r" + i]
+          if (ele !== undefined) {
+            references.push({
+              imageUrl: ele["image-ref"],
+              recipeName: ele["name"]
+            })
+          }
+        }
+
+        this.setState({
+          wasResult: true,
+          mainResult: {
+            recipeName: query,
+            directions: "Directions: " + res.data["directions"],
+            ingredients: "Ingredients: " + res.data["ingredients"]["text"],
+            imageUrl: "" + res.data["image"]
+          },
+          referenceResult: references
+        });
+      });
+  }
+
+  componentWillReceiveProps(props: ResultContainer) {
+    if (props.query.length > 0) {
+      this.doSearch(props.query)
+    } else {
+      this.setState({ wasResult: undefined, query: "" })
     }
+  }
+
+  render() {
+    if (this.state.wasResult === undefined) {
+      return (<div></div>);
+    }
+    else if (this.state.wasResult === false) {
+      return (<h2>No search result found for {this.state.query}</h2>)
+    }
+    else {
+      const myThis = this;
+      return (
+        <div>
+          <MainResult result={this.state.mainResult} />
+          <ReferencedResults tiles={this.state.referenceResult} changeRecipe={(recipeName:string) => { myThis.doSearch(recipeName); }}/>
+        </div>
+      );
+    }
+  }
 }
+
+
--- flask-dashboard/Application/src/WebView.Component.tsx
+++ flask-dashboard/Application/src/WebView.Component.tsx
@@ -3,6 +3,20 @@ import SearchBar from 'material-ui-search-bar';
 import MainResult from './MainResult.Component';
 
 
+interface ConditionalTitleProps {
+    searchValue: string
+}
+class ConditionalTitle extends React.Component<ConditionalTitleProps> {
+    render() {
+        if (this.props.searchValue.length > 0) {
+            return (<div></div>)
+        } else {
+            return (<h1> Recipe Revealer </h1>)
+        }
+    }
+}
+
+
 
 interface SearchBarProps {
     parent: React.Component
@@ -19,19 +33,22 @@ class TheSearchBar extends React.Component<SearchBarProps> {
     }
     render() {
         return (
-            <div className=".searchContainer">
-                <SearchBar
-                    placeholder = "Search a Recipe!"
-                    id="searchBar"
-                    className="search"
-                    onChange={(value) => { this.setState({ searchValue: value }) }}
-                    onRequestSearch={() => { this.searchWasRequested() }}
-                    style={{
-                        margin: '4em auto',
-                        maxWidth: 800,
-                        backgroundColor: 'beige'
-                    }}
-                />
+            <div>
+                <ConditionalTitle searchValue={this.state.searchValue} />
+                <div className=".searchContainer">
+                    <SearchBar
+                        placeholder="Search a Recipe!"
+                        id="searchBar"
+                        className="search"
+                        onChange={(value) => { this.setState({ searchValue: value }) }}
+                        onRequestSearch={() => { this.searchWasRequested() }}
+                        style={{
+                            margin: '4em auto',
+                            maxWidth: 800,
+                            backgroundColor: 'beige'
+                        }}
+                    />
+                </div>
             </div>
         );
     }
@@ -49,9 +66,9 @@ export default class WebView extends React.Component {
         return (
             <div>
                 <TheSearchBar parent={this} />
-            <div id="searchResult">
-                  <MainResult recipeName={this.state.queryString} />
-            </div>
+                <div id="searchResult">
+                    <MainResult query={this.state.queryString} />
+                </div>
             </div>
         )
     }
